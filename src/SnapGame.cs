1751366d16544d143ca17232247456cf874d5f8f
John <john.seymour787@gmail.com>
2019-04-04 08:57:25 +1100

    This is an existing branch that we will conflict with

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -56,10 +56,10 @@ namespace CardGames
             Card top = myGame.TopCard;
             if (top != null)
             {
-                SwinGame.DrawText("Top Card is " + top.ToString(), Color.RoyalBlue, "GameFont", 0, 20);
-                SwinGame.DrawText("Top Card is " + top.ToString(), Color.RoyalBlue, "GameFont", 0, 20);
-                SwinGame.DrawText("Player 1 score: " + myGame.Score(0), Color.Red, "GameFont", 0, 30);
-                SwinGame.DrawText("Player 2 score: " + myGame.Score(1), Color.Green, "GameFont", 0, 40);
+                SwinGame.DrawText("Top Card is " + top.ToString(), Color.Green, "GameFont", 0, 20);
+                SwinGame.DrawText("Top Card is " + top.ToString(), Color.Red, "GameFont", 0, 20);
+                SwinGame.DrawText("Player 1 score: " + myGame.Score(0), Color.Blue, "GameFont", 0, 30);
+                SwinGame.DrawText("Player 2 score: " + myGame.Score(1), Color.Yellow, "GameFont", 0, 40);
                 SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), top.CardIndex, 350, 50);
             }
             else
